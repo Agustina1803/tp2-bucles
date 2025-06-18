3c047e61e3b18c1d03bb2be2393f87d95c4db702
AgusBulacio <agusbulacio1803@gmail.com>
2025-06-18 18:37:23 -0300

    agrege la logica del problema 17 del tp 2

---
+++ README.md
@@ -0,0 +1,21 @@
+# Tp2️⃣ - Bucles ✨
+
+Práctica de JavaScript 💻
+
+## Tecnologías utilizadas 
+
+- HTML5
+- CSS3
+- [Bootstrap v5.3](https://getbootstrap.com/)
+- JavaScript
+
+
+## Cómo ejecutar el proyecto
+
+1. Clonar el repositorio escribiendo en una terminal el siguiente comando `https://github.com/Agustina1803/tp2-bucles.git`
+1. Luego abrí el index.html con un navegador y cambiar el src en el script para cada uno de los ejercicios de la carpeta js/ejercicios
+
+
+## Autores
+
+- Bulacio Agustina 👩🏻‍💻
--- js/ejercicios/ej17-posicionVocales.js
+++ js/ejercicios/ej17-posicionVocales.js
@@ -1,10 +1,24 @@
 const texto = prompt(`Ingrese un texto: `);
-const vocales =`aeiou`;
+const vocales =`aeiouAEIOU`;
+let primeraVocal;
+let posicionVocal;
 
 for (i=0 ; i< texto.length; i++){
     for (j=0; j< vocales.length; j++){
         if (texto[i] === vocales[j]){
-            document.writeln(`La posicion de la vocal ${texto[i]} esta en posicion ${i} `);
+            primeraVocal = texto[i]; 
+            posicionVocal = i;
+            console.log(primeraVocal, posicionVocal);
+            break;
         }
     }
+    if (primeraVocal !=  null){
+        break;
+    }
 }
+
+if (primeraVocal != null && posicionVocal != null){
+    console.log(primeraVocal,posicionVocal);
+    document.writeln(`La posicion de la vocal ${primeraVocal} es ${posicionVocal}`);
+}
+
